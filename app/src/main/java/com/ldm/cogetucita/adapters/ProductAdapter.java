a6edbad34ed7899db21a2669d3b1f1b3e88ea19a
Jorge Galindo Peña <jorgape225@gmail.com>
2020-11-09 20:04:43 +0100

    Added new icon

--- app/src/main/java/com/ldm/cogetucita/adapters/ProductAdapter.java
+++ app/src/main/java/com/ldm/cogetucita/adapters/ProductAdapter.java
@@ -1,9 +1,6 @@
 package com.ldm.cogetucita.adapters;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +8,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.recyclerview.widget.RecyclerView;
-import com.ldm.cogetucita.BuildConfig;
 import com.ldm.cogetucita.MainActivity;
 import com.ldm.cogetucita.R;
 import com.ldm.cogetucita.models.Product;
-
-import java.io.File;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
     private MainActivity mainActivity;
