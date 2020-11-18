72e6d693bc87a9146448c85edcdb868baca393e4
JorgeGalingP <jorgape225@gmail.com>
2020-11-18 22:55:49 +0100

    HelpActivity WIP
    Added first capture

--- app/src/main/java/com/ldm/cogetucita/fragments/Help1Fragment.java
+++ app/src/main/java/com/ldm/cogetucita/fragments/Help1Fragment.java
@@ -1,6 +1,9 @@
 package com.ldm.cogetucita.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,7 +24,15 @@ public class Help1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help1, container, false);
+        // inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_help1, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.help_1);
+
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+
+        return view;
     }
 }
